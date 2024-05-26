3ec7ebc347884dae66d98d9484102388694c1458
Sergio Durán <sergio@tiene.info>
2024-05-25 19:04:17 -0600

    Mejorar la vista de info de rasgos

--- src/routes/play/TraitInfo.svelte
+++ src/routes/play/TraitInfo.svelte
@@ -33,4 +33,11 @@ let modal = $state(false)
 <Modal onclose={() => modal = false} body={info} />
 {/if}
 <button class="btn btn-info btn-sm p-0" onclick={()=> modal=true}><span class="bi bi-info-circle"></span></button>
-<TraitIcon trait={trait.trait} /> {trait.trait.name} nível {trait.level+1}
+<TraitIcon trait={trait.trait} /> {trait.trait.name} <span class="badge bg-secondary">{trait.active}</span>
+{#each trait.levels as level, i}
+	{#if i == trait.level}
+		<b>{level.amount}</b> >
+	{:else}
+		{level.amount} >
+	{/if}
+{/each}
