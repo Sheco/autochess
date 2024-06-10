8e21bbd9186882ea65fd2e0010a029373755c512
Sergio Durán <sergio@tiene.info>
2024-06-10 10:52:27 -0600

    Arreglar el dialogo de rasgos

--- src/lib/TraitInfo.svelte
+++ src/lib/TraitInfo.svelte
@@ -9,10 +9,8 @@ let { trait }: {
 
 let modal = $state(false)
 </script>
-{#snippet info()}
-	<div class="card">
-		<div class="card-header"><TraitIcon trait={trait.trait} /> {trait.trait.name} nível {trait.level+1}</div>
-		<div class="card-body">
+{#if modal}
+<Modal onclose={() => modal = false} title="{trait.trait.icon} {trait.trait.name} nível {trait.level+1}">
 			<p>{@html trait.message}</p>
 			{#each trait.mods as mod}
 				Aplica a <TraitIcon trait={mod.target} />:
@@ -26,11 +24,7 @@ let modal = $state(false)
 				{/if}
 				<br>
 			{/each}
-		</div>
-	</div>
-{/snippet}
-{#if modal}
-<Modal onclose={() => modal = false} body={info} />
+</Modal>
 {/if}
 <button class="btn btn-info btn-sm p-0" onclick={()=> modal=true}><span class="bi bi-info-circle"></span></button>
 <TraitIcon trait={trait.trait} /> {trait.trait.name} <span class="badge bg-secondary">{trait.active}</span>
